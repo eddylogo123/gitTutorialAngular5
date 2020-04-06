5a6af0df4fd50f706aa569206b8075bb989581b8
artemisalogo <artemisa.logo@hotmail.com>
2020-04-06 09:07:21 -0400

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
